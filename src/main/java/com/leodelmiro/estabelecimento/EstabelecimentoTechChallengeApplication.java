df880228e2d5908e6d4da573c035751659e9415a
leodelmiro <leodelmiroms@gmail.com>
2024-06-30 20:25:42 -0300

    feat: Project base commit

---
+++ src/main/java/com/leodelmiro/estabelecimento/EstabelecimentoTechChallengeApplication.java
@@ -0,0 +1,13 @@
+package com.leodelmiro.estabelecimento;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EstabelecimentoTechChallengeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EstabelecimentoTechChallengeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/leodelmiro/estabelecimento/EstabelecimentoTechChallengeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.leodelmiro.estabelecimento;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EstabelecimentoTechChallengeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
