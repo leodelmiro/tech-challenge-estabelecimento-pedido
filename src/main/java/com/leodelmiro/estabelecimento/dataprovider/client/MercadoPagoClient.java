158f85bbff95cf4c45714eecbe817deeb076f7b8
Leonardo Delmiro <leodelmiroms@gmail.com>
2024-10-02 15:06:02 -0300

    refactor: removendo log debug

--- src/main/java/com/leodelmiro/estabelecimento/dataprovider/client/MercadoPagoClient.java
+++ src/main/java/com/leodelmiro/estabelecimento/dataprovider/client/MercadoPagoClient.java
@@ -1,6 +1,5 @@
 package com.leodelmiro.estabelecimento.dataprovider.client;
 
-import com.leodelmiro.estabelecimento.config.MercadoPagoClientConfig;
 import com.leodelmiro.estabelecimento.dataprovider.client.request.PedidoClientRequest;
 import com.leodelmiro.estabelecimento.dataprovider.client.response.QrCodeResponse;
 import org.springframework.cloud.openfeign.FeignClient;
@@ -9,7 +8,7 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestHeader;
 
-@FeignClient(name = "mercadoPagoClient", url = "${external-apis.mercadopago.url}", configuration = MercadoPagoClientConfig.class)
+@FeignClient(name = "mercadoPagoClient", url = "${external-apis.mercadopago.url}")
 public interface MercadoPagoClient {
     @PostMapping("instore/orders/qr/seller/collectors/{vendedorId}/pos/{caixaId}/qrs")
     QrCodeResponse gerarQrCode(@PathVariable String vendedorId,
